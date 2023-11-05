f204c642ddba40e937fde4044114b914b745ce8a
Mertnode <mertnode@gmail.com>
2023-11-05 23:35:29 +0300

    password hashleme islemi yapildi

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,10 +11,10 @@ exports.registerUser = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error("Please fill in all required fields")
     } 
-    if (password.lenght < 6) {
-        res.status(400)
-        throw new Error("Password must be up to 6 characters")
-    }s
+    if (password.length < 6) {
+        res.status(400);
+        throw new Error("Şifre en az 6 karakter olmalıdır");
+    }
 
     // Check if user email already exists
     const userExists = await User.findOne({email})
@@ -22,7 +22,7 @@ exports.registerUser = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error("Email has already been registered")
     }
-
+    
     //Create new user
     const user = await User.create({
         name,
@@ -32,7 +32,7 @@ exports.registerUser = asyncHandler(async (req,res) => {
     if (user) {
         const {_id,name,email,photo,phone,bio} = user
         res.status(201).json({
-           _id,name,email,photo,phone,bio
+           _id,password,name,email,photo,phone,bio
         })
     } else {
         res.status(400)
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,37 +1,52 @@
-const mongoose = require('mongoose')    
-const userSchema = mongoose.Schema({
-    name : {
-        type : String,
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
         required: [true, "Please add a name"]
     },
-    email : {
-        type : String,
-        required: [true, "Please add a email"],
-        uniqe:true,
-        trim:true,
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        trim: true,
         match: [
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please enter a valid email"
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
         ]
     },
-    password : {
-        type : String,
+    password: {
+        type: String,
         required: [true, "Please add a password"],
-        minLenght:[6,"Password must be up to 6 characters"],
-        maxLength:[23,"Password must be more than 23 characters "]
+        minlength: [6, "Password must be at least 6 characters"],
+        //maxlength: [50, "Password must be less than 50 characters"]
     },
-    photo : {
-        type : String,
+    photo: {
+        type: String,
         required: [true, "Please add a photo"],
         default: "https://cdn-icons-png.flaticon.com/512/3177/3177440.png"
     },
-    phone : {
-        type : String,
-        default:"0"
+    phone: {
+        type: String,
+        default: "0"
     },
-    bio : {
-        type : String,
-        default:"0"
+    bio: {
+        type: String,
+        default: "0"
     },
-    
-},{timestamps:true})
-module.exports =  mongoose.model( 'User' , userSchema)
+}, { timestamps: true });
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
