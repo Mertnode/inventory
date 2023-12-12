ff5fc0861434f82668b3511d111a8ac103d03cef
Mertnode <mertnode@gmail.com>
2023-12-13 00:36:27 +0300

    ayarlar

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,18 @@
-const asyncHandler = require("express-async-handler")
-const Product = require("../models/productModel")
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
 
-exports.createProduct = asyncHandler(async (req,res) => {
-    const {name, sku, category, quantity, price, description} = req.body
+exports.createProduct = asyncHandler(async (req, res) => {
+    const { name, sku, category, quantity, price, description } = req.body;
 
-    //Validation
-    if (!name || !category || !price || !quantity || !description ) {
-        res.status(400).json({success:false, message:"Please fill in all fields"})
+    // Validation
+    if (!name || !category || !price || !quantity || !description || !sku) {
+        return res.status(400).json({
+            success: false,
+            message: "Please fill in all fields",
+        });
     }
 
-    //Create Product
+    // Create Product
     const product = await Product.create({
         user: req.user.id,
         name,
@@ -18,7 +21,7 @@ exports.createProduct = asyncHandler(async (req,res) => {
         quantity,
         price,
         description,
-    })
-    res.status(201).json(product
-    )
-})
+    });
+
+    res.status(201).json(product);
+});
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,46 +1,48 @@
 const mongoose = require("mongoose");
 
-
-
-// noinspection JSValidateTypes
 const productSchema = mongoose.Schema({
-    user:{
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:"user"
+        required: true,
+        ref: "user"
     },
-    name : {
+    name: {
         type: String,
-        required: [true , "Please add a name"],
-        trim:true
-    },
-    sku : {
-        type:String,
-        required:true,
-        default:"SKU",
+        required: [true, "Lütfen bir isim ekleyin"],
         trim: true
     },
-    category : {
-        type:String,
-        requried:[true,"Please add a category"],
+    sku: {
+        type: String,
+        required: true,
+        default: "SKU",
         trim: true
     },
-    quantity : {
-        type:String,
-        requried: [true,"Please add a quantity"],
-        trim : true
+    category: {
+        type: String,
+        required: [true, "Lütfen bir kategori ekleyin"],
+        trim: true
     },
-    description : {
-        type:String,
-        requried:[true,"Please add a description"],
-        trim : true
+    quantity: {
+        type: String,
+        required: [true, "Lütfen bir miktar ekleyin"],
+        trim: true
     },
-    image : {
-        type:Object,
+    description: {
+        type: String,
+        required: [true, "Lütfen bir açıklama ekleyin"],
+        trim: true
+    },
+    image: {
+        type: Object,
         default: {}
+    },
+    price: {
+        type: String,
     }
-},{
-    timestamps:true
-})
-const Product = mongoose.model("Product",productSchema)
-module.exports = Product
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
